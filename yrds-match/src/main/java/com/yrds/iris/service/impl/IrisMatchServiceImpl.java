27fd05ea14e7a6d7e4b775faa7508468efb330ab
Michael <michael_0877@163.com>
2018-03-21 16:44:24 +0800

    添加工程yrds-match
    Change-Id: Ibdf7274529af630f91ef93ea6e0d0add65f0959f
    Signed-off-by: Michael <michael_0877@163.com>

---
+++ yrds-match/src/main/java/com/yrds/iris/service/impl/IrisMatchServiceImpl.java
@@ -0,0 +1,42 @@
+package com.yrds.iris.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kivi.framework.db.page.PageInfoKtf;
+import com.kivi.framework.service.KtfNameService;
+import com.kivi.framework.vo.page.PageReqVO;
+import com.yrds.iris.dao.IrisFeatureDao;
+import com.yrds.iris.dto.IrisMatch;
+import com.yrds.iris.dto.IrisMatchRsp;
+import com.yrds.iris.persist.model.IrisFeature;
+import com.yrds.iris.service.IrisMatchService;
+
+@Service
+public class IrisMatchServiceImpl implements IrisMatchService {
+
+    @Autowired
+    private KtfNameService ktfNameService;
+
+    @Autowired
+    private IrisFeatureDao irisFeatureDao;
+
+    @Override
+    public IrisMatchRsp match( IrisMatch req ) {
+        int slotId = ktfNameService.index();
+        int slotCount = ktfNameService.countOnline();
+        int offset = 0;
+        PageReqVO pageReq = new PageReqVO(500, offset, null, null);
+
+        PageInfoKtf<IrisFeature> page = null;
+        do {
+            page = irisFeatureDao.listByPage(null, slotId, slotCount, pageReq);
+
+            // TODO 添加虹膜匹配的代码
+        }
+        while (page != null && !page.getIsLastPage());
+
+        return null;
+    }
+
+}
