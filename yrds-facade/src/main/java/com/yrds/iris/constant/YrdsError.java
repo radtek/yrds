a0bad847516f396a181b3f2f34f2e855f25c04e4
Michael <michael_0877@163.com>
2018-03-23 11:00:06 +0800

    增加错误码
    Change-Id: I06acde9293d73d8ac2d2de60501c4e215f2d60d1
    Signed-off-by: Michael <michael_0877@163.com>

--- yrds-facade/src/main/java/com/yrds/iris/constant/YrdsError.java
+++ yrds-facade/src/main/java/com/yrds/iris/constant/YrdsError.java
@@ -8,4 +8,14 @@ public class YrdsError extends GlobalErrorConst {
 
     /** 虹膜数据为空 */
     public static final String E_IR_NO_IRIS              = "ER000002";
+
+    /** 虹膜SDK初始化失败 */
+    public static final String E_IR_SDK_INIT             = "ER000003";
+
+    /** 虹膜SDK创建模板失败 */
+    public static final String E_IR_SDK_CREATE_TEMPLETE  = "ER000004";
+
+    /** 虹膜SDK比对失败 */
+    public static final String E_IR_SDK_MATCH_IRIS       = "ER000005";
+
 }
